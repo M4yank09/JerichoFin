b8dc3db19cb59a66beacd685dd08f66bc3f4c6db
Volt <87929019+M4yank09@users.noreply.github.com>
2026-09-06 00:39:31 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
-from app.api.v1 import api_router
+from backend.app.api.v1 import api_router
 from backend.app.core.config import settings
 
 
